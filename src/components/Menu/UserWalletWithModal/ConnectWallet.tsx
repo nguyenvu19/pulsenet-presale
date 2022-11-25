fc262db6bb2f1cfd266334c69960abc8b1d75e88
DavidNgo <ngohung2393@gmail.com>
2022-11-25 08:30:47 +0700

    update: wallet icon

--- src/components/Menu/UserWalletWithModal/ConnectWallet.tsx
+++ src/components/Menu/UserWalletWithModal/ConnectWallet.tsx
@@ -10,7 +10,7 @@ export const StyledUserMenu = styled(Flex)`
   width: 170px;
   height: 44px;
   padding: 4px 8px;
-  padding-left: 50px;
+  padding-left: 40px;
   margin-left: 0;
   position: relative;
 
@@ -22,6 +22,14 @@ export const StyledUserMenu = styled(Flex)`
   background-position: left center;
   ${({ theme }) => theme.mediaQueries.sm} {
     width: 288px;
+    padding-left: 46px;
+  }
+
+  img {
+    width: 30px;
+    height: auto;
+    object-fit: contain;
+    margin-right: 6px;
   }
 `
 export const LabelText = styled.div`
@@ -50,6 +58,7 @@ const ConnectWallet = ({ account, onPresentWalletModal, ...props }: any) => {
   const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null
   return (
     <StyledUserMenu onClick={handleClick} {...props}>
+      {accountEllipsis && <img src="/images/user_wallet_icon.png" alt="" />}
       <LabelText>{accountEllipsis || 'Connect Wallet'}</LabelText>
     </StyledUserMenu>
   )
