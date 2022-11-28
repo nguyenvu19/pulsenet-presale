e7dc473d891e789e29a9e16cfa4c72e118b280a7
dilysngo <ngohung2393@gmail.com>
2022-11-28 23:43:14 +0700

    update: multichain list

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -9,7 +9,7 @@ import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi'
 
-const CHAINS = process.env.NODE_ENV === 'production' ? [bsc, mainnet] : [bsc, mainnet, bscTest, rinkeby, goerli]
+const CHAINS = process.env.NODE_ENV === 'production' ? [bsc, mainnet] : [bscTest, goerli]
 
 const getNodeRealUrl = (networkName: string) => {
   let host = null
