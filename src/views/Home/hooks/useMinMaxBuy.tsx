f71b646df04b92e81d722f0cf6712364c6f27191
dilysngo <ngohung2393@gmail.com>
2022-12-01 13:32:47 +0700

    update

--- src/views/Home/hooks/useMinMaxBuy.tsx
+++ src/views/Home/hooks/useMinMaxBuy.tsx
@@ -1,30 +1,50 @@
 import { useState, useEffect } from 'react'
 import { useSellPullContract } from 'hooks/useContract'
 import { formatBigNumber } from 'utils/formatBalance'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
 
 const useMinMaxBuy = () => {
+  const { chainId } = useActiveWeb3React()
   const [value, setValue] = useState({
-    min: undefined,
-    max: undefined,
+    1: {
+      min: undefined,
+      max: undefined,
+    },
+    5: {
+      min: undefined,
+      max: undefined,
+    },
+    56: {
+      min: undefined,
+      max: undefined,
+    },
+    97: {
+      min: undefined,
+      max: undefined,
+    },
   })
+
   const contractSellPull = useSellPullContract(false)
 
   useEffect(() => {
     const getData = async () => {
       try {
         const [min, max] = await Promise.all([contractSellPull.MIN_BUY(), contractSellPull.MAX_BUY()])
-        setValue({
-          min: +formatBigNumber(min),
-          max: +formatBigNumber(max),
-        })
+        setValue((prev) => ({
+          ...prev,
+          [chainId]: {
+            min: +formatBigNumber(min),
+            max: +formatBigNumber(max),
+          },
+        }))
       } catch (e) {
         console.error('fetch min max error', e)
       }
     }
     getData()
-  }, [contractSellPull])
+  }, [chainId, contractSellPull])
 
-  return value
+  return value[chainId]
 }
 
 export default useMinMaxBuy
