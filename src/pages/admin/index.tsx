2afda35c494ec4e202cd858eefd9195ad91e058a
Nguyen Vu <nguyenvu191000@gmail.com>
2022-12-23 16:53:35 +0700

    add view sale

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,10 @@
+import AdminHomePage from 'views/Admin/Home'
+import AdminLayout from 'components/Admin/Layout'
+
+const Admin = () => {
+  return <AdminHomePage />
+}
+
+Admin.mp = true
+Admin.Layout = AdminLayout
+export default Admin
